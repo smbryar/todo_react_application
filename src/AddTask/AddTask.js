6e2b5e01a93fdc9fda40a0c5aad765f080a57fa3
Saoirse Bryar <smbryar@gmail.com>
2020-05-05 13:52:46 +0100

    Format repeating task in Add Task

--- src/AddTask/AddTask.js
+++ src/AddTask/AddTask.js
@@ -9,87 +9,120 @@ function AddTask(props) {
   const [open, setOpen] = React.useState(false);
   return (
     <Card className="add-task">
-    <Card.Header
-      as="h4"
-      onClick={() => setOpen(!open)}
-      aria-controls="add-task-contents"
-      aria-expanded={open}
-    >
-      Add Task
+      <Card.Header
+        as="h4"
+        onClick={() => setOpen(!open)}
+        aria-controls="add-task-contents"
+        aria-expanded={open}
+      >
+        Add Task
     </Card.Header>
 
-    <Collapse in={open}>
-          <Card.Body>
+      <Collapse in={open}>
+        <Card.Body>
           <div id="add-task-contents">
-          <div className="row">
-            
-            <div className="col-10">
-              <Form>
-                <Form.Group controlId="formTaskName">
-                  <Form.Label>Add new task</Form.Label>
-                  <Form.Control type="text" placeholder="Name your next task" />
-                </Form.Group>
-              </Form>
-            </div>
-            
-            <div className="col-2">
-              <div className="float-right">
-                <ButtonItem type="submit" hoverText="Add Task">
-                  <AddIcon squareHeight="2em"/>
-                </ButtonItem>
-            </div>
-            </div>
-          
-          </div>
-          
-          <div className="row">
-            <div className="col-6">
-              <Form>
-                <Form.Group controlId="formStartDate">
-                  <Form.Label>Start date</Form.Label>
-                  <Form.Control type="date"/>
-                </Form.Group>
-              </Form>
-            </div>
-            <div className="col-6">
-              <Form>
-                <Form.Group controlId="formEndDate">
-                  <Form.Label>End date</Form.Label>
-                  <Form.Control type="date"/>
-                </Form.Group>
-              </Form>
-            </div>
-          </div>
-        </div>
-        
-        <Form>
-          <Form.Group controlId="formRepeatSwitch">
-            <Form.Check type="switch" label="Repeat Task"/> 
-          </Form.Group>
-            <Form.Row>
-            <Form.Label column lg={2}>Repeats every</Form.Label>
-              <div className="col-2">                
-                <Form.Control type="number"/>
+            <div className="row">
+
+              <div className="col-10">
+                <Form>
+                  <Form.Group controlId="formTaskName">
+                    <Form.Label>Add new task</Form.Label>
+                    <Form.Control type="text" placeholder="Name your next task" />
+                  </Form.Group>
+                </Form>
               </div>
+
               <div className="col-2">
-              <Form.Control as="select" custom>
-              <option>days</option>
-              <option>weeks</option>
-              <option>months</option>
-              <option>years</option>
-            </Form.Control>
-            
+                <div className="float-right">
+                  <ButtonItem type="submit" hoverText="Add Task">
+                    <AddIcon squareHeight="2em" />
+                  </ButtonItem>
+                </div>
+              </div>
+
             </div>
-              <Form.Label column lg={6}>
-                after the task has been completed.
-              </Form.Label>
+
+            <div className="row">
+              <div className="col-6">
+                <Form>
+                  <Form.Group controlId="formStartDate">
+                    <Form.Label>Start date</Form.Label>
+                    <Form.Control type="date" />
+                  </Form.Group>
+                </Form>
+              </div>
+              <div className="col-6">
+                <Form>
+                  <Form.Group controlId="formEndDate">
+                    <Form.Label>End date</Form.Label>
+                    <Form.Control type="date" />
+                  </Form.Group>
+                </Form>
+              </div>
+            </div>
+          </div>
+
+          <Form>
+            <Form.Row className="customRow">
+              <Form.Check type="radio" name="repeatChoice" className="align-self-center" />
+              <div className="col align-self-center">
+                <Form.Label className="noBottonMargin">Does not repeat</Form.Label>
+              </div>              
             </Form.Row>
-            
 
-        </Form>
+            <Form.Row className="customRow">
+              <Form.Check type="radio" name="repeatChoice" className="align-self-center"/>
+              <div className="col-4 col-md-2 align-self-center">
+                <Form.Label className="noBottonMargin">Repeats every</Form.Label>
+              </div>
 
-                  </Card.Body>
-        
+              <div className="col-3 col-md-1">
+                <Form.Control type="number" />
+              </div>
+              <div className="col-4 col-md-2">
+                <Form.Control as="select" custom>
+                  <option>days</option>
+                  <option>weeks</option>
+                  <option>months</option>
+                  <option>years</option>
+                </Form.Control>
+
+              </div>
+              <div className="col-12 col-md-6 align-self-center">
+              <Form.Label className="noBottonMargin">after the task has been completed.</Form.Label>
+              </div>
+              
+            </Form.Row>
+            <Form.Row className="customRow">
+              <Form.Check type="radio" name="repeatChoice" className="align-self-center"/>
+              <div className="col-4 col-md-2 align-self-center">
+                <Form.Label className="noBottonMargin">Repeats every</Form.Label>
+              </div>              
+              <div className="col-3 col-md-1">
+                <Form.Control type="number" />
+              </div>
+              <div className="col-4 col-md-2 align-self-center">
+                <Form.Label className="noBottonMargin">weeks on</Form.Label>
+              </div>
+              <div className="col-12 col-md-6 align-self-center">
+                <div className="row">
+                  {["M", "T", "W", "T", "F", "S", "S"].map(day => (
+                <div key="inline-checkbox">
+                  <Form.Check inline label={day} />
+                </div>
+              ))}
+                </div>
+                
+              </div>              
+              
+
+            </Form.Row>
+
+
+          </Form>
+
+        </Card.Body>
+
       </Collapse>
     </Card>
   );
