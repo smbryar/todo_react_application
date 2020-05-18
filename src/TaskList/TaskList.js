08892aca81ea63f6c6ebd6ece373346756b01524
Saoirse Bryar <smbryar@gmail.com>
2020-05-18 13:27:36 +0100

    Clear add task after submitting

--- src/TaskList/TaskList.js
+++ src/TaskList/TaskList.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
+import { v4 as uuidv4 } from 'uuid';
 import TaskItem from '../TaskItem/TaskItem';
 import AddTask from '../AddTask/AddTask';
 import Intro from '../Intro/Intro';
 import './TaskList.css';
 
 function TaskList(props) {
+
+    const [addTaskKey, setAddTaskKey] = useState(uuidv4());
+
     function compare(a, b) {
         if (a.completed === true || a.startDate > moment().format("YYYY-MM-DD") || a.endDate > b.endDate) return 1;
         if (a.endDate < b.endDate || a.startDate < b.startDate) return -1;
         return 0;
     }
+    
+    function addTask(name, taskDetails, startDate, endDate, repeats, repeatType, repeatAfterCompletionFrequency,
+        repeatAfterCompletionFrequencyType,repeatRegularDaysFrequency,repeatRegularDaysArrayDays){
+
+            setAddTaskKey(uuidv4());
+
+            props.addTask(name, taskDetails, startDate, endDate, repeats, repeatType, repeatAfterCompletionFrequency,
+            repeatAfterCompletionFrequencyType,repeatRegularDaysFrequency,repeatRegularDaysArrayDays);
+        }
 
     return (
         <div className="col-12 col-lg-8">
@@ -21,7 +34,7 @@ function TaskList(props) {
                 </div>
 
                 <div className="row my-2">
-                    <AddTask addTask = {props.addTask} tasks = {props.tasks}/>
+                    <AddTask key={addTaskKey} addTask = {addTask} tasks = {props.tasks}/>
                 </div>
 
 
