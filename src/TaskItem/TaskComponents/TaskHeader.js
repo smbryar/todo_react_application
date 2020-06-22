ac5c08bde48f7f183a7e79ff2224fb935a9890f4
Saoirse Bryar <smbryar@gmail.com>
2020-06-22 10:24:11 +0100

    Remove tooltips for buttons due to issue where they sometimes misalign

--- src/TaskItem/TaskComponents/TaskHeader.js
+++ src/TaskItem/TaskComponents/TaskHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, OverlayTrigger, Tooltip, Row, Col, ButtonGroup } from 'react-bootstrap';
+import { Card, Row, Col, ButtonGroup } from 'react-bootstrap';
 
 import ButtonItem, { CheckboxIcon, DeleteIcon, RepeatIcon } from '../../ButtonItem/ButtonItem';
 import TaskProgressBar from './TaskProgressBar';
@@ -10,7 +10,7 @@ function TaskHeader(props) {
   function handleHeaderClick() {
     props.openTaskCard(props.taskID);
   }
-  
+
   return (
     <Card.Header
       className="h4"
@@ -19,7 +19,7 @@ function TaskHeader(props) {
     >
       <Row>
         <Col xs={8}
-          style={{ textDecoration: props.completed ? 'line-through' : '' ,cursor: "pointer"}}
+          style={{ textDecoration: props.completed ? 'line-through' : '', cursor: "pointer" }}
           tabIndex="0"
           onClick={handleHeaderClick}
           onKeyPress={(target) => target.charCode === 13 ? props.openTaskCard(props.taskID) : null}>
@@ -29,17 +29,12 @@ function TaskHeader(props) {
         <Col xs={4}>
           <ButtonGroup className="float-right">
             {!props.completed &&
-              <OverlayTrigger placement="top" overlay={<Tooltip>Mark as complete</Tooltip>}>
-                <ButtonItem onClick={props.handleCompleteClick} aria-label="Mark as complete">
-                  <CheckboxIcon squareHeight="1.5em" />
-                </ButtonItem>
-              </OverlayTrigger>}
-
-            <OverlayTrigger placement="top" overlay={<Tooltip>Delete</Tooltip>}>
-              <ButtonItem onClick={props.handleDeleteClick} aria-label="Delete">
-                <DeleteIcon squareHeight="1.3em" />
-              </ButtonItem>
-            </OverlayTrigger>
+              <ButtonItem onClick={props.handleCompleteClick} aria-label="Mark as complete">
+                <CheckboxIcon squareHeight="1.5em" />
+              </ButtonItem>}
+            <ButtonItem onClick={props.handleDeleteClick} aria-label="Delete">
+              <DeleteIcon squareHeight="1.3em" />
+            </ButtonItem>
           </ButtonGroup>
         </Col>
       </Row>
