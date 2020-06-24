19e6c0acb355eabf40468f4385fa5f991590f9a8
Saoirse Bryar <smbryar@gmail.com>
2020-06-24 12:27:10 +0100

    Updates

--- precache-manifest.b673ddbc1904912f4619fbce250957f2.js
+++ precache-manifest.b673ddbc1904912f4619fbce250957f2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb051dfe54018e96b52850879fb4fbfd",
+    "revision": "3721755ebe1602010f163699dd54d5d5",
     "url": "/todo_react_application/index.html"
   },
   {
-    "revision": "ff992b8636a3a98669af",
+    "revision": "95f93681fc81c079364e",
     "url": "/todo_react_application/static/css/main.6ff0760c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/todo_react_application/static/js/2.362a14a6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ff992b8636a3a98669af",
-    "url": "/todo_react_application/static/js/main.d1e33d6d.chunk.js"
+    "revision": "95f93681fc81c079364e",
+    "url": "/todo_react_application/static/js/main.97a1dc6c.chunk.js"
   },
   {
     "revision": "5d4c1a4e2e9a73a8502e",
