bcd04332b7a637600e782afedac9d376110d105e
Saoirse Bryar <smbryar@gmail.com>
2020-06-26 13:44:53 +0100

    Add AWS amplify to app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Amplify} from 'aws-amplify';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import config from 'config';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
